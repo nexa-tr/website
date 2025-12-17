26c6e740eb3ed2a211351ed2b92c6931b82768d3
kaan-w <105508453+kaan-w@users.noreply.github.com>
2025-12-17 20:41:02 +0300

    change defaults

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,21 +9,36 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			plugins: [starlightBlog()],
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			customCss: ['./src/styles/custom.css'],
+			title: 'NEXA Topluluğu',
+			social: [
+				{ icon: 'instagram', label: 'Instagram', href: 'https://instagram.com/nexatr0' },
+				{ icon: 'linkedin', label: 'LinkedIn', href: 'https://linkedin.com/company/nexatr' },
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/nexa-tr' }
+			],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Duyurular',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Ekip alımları', slug: 'announcements/recruitment' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Hakkımızda',
+					items: [
+						{ label: 'Üyelerimiz', slug: 'info/members' },
+						{ label: 'Biz Kimiz', slug: 'info/who-we-are' },
+						{ label: 'İletişim', slug: 'info/contact' },
+					],
 				},
-			],
+				{
+					label: 'Projeler',
+					items: [
+						{ label: 'Proje 1', slug: 'projects/project-1' },
+						{ label: 'Proje 2', slug: 'projects/project-2' },
+					],
+				},
+			]
 		}),
 	],
 });
