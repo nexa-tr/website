c275527012b10ae93902f12a1f228cf2dc968360
Görkem Paradise <smailgorkem@gmail.com>
2026-01-07 02:35:49 +0300

    Some draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,7 +40,7 @@ export default defineConfig({
 						{ label: 'Proje 3', slug: 'projeler/proje-3' },
 						{ label: 'Proje 4', slug: 'projeler/proje-4' },
 					],
-				},
+				},			
 			]
 		}),
 	],
